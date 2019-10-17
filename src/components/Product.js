838e3a53ae467d995f824ffcfa108cca1ea06585
James Hall <halljames631@gmail.com>
2019-10-17 09:28:29 -0400

    added className insted of class

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,7 @@ const Product = ({ product }) => (
     <div className="card col-4">
     <img src={product.img} className="card-img-top" alt="..."/>
     <div className="card-body">
-      <h5 className="card-title">{product.name} <span class="badge badge-success">{product.inStock} inStock</span></h5>
+      <h5 className="card-title">{product.name} <span className="badge badge-success">{product.inStock} inStock</span></h5>
       <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <Link to={`/products/${product.id}`} className="btn btn-primary">Go somewhere</Link>
     </div>
